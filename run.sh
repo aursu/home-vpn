5f775ddbf1c5490fb70f2e25a9578cc0c4c5746a
Alexander Ursu <alexander.ursu@gmail.com>
2020-01-28 22:10:02 +0100

    VPN run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+config=PUREVPN.ovpn
+if [ -n "$1" -a -f "$1" ]; then
+  config="$1"
+fi
+
+export OPENSSL_ENABLE_MD5_VERIFY=1
+openvpn --config "$config" \
+        --auth-user-pass login.conf \
+        --setenv OPENSSL_ENABLE_MD5_VERIFY 1 \
+        --daemon openvpn \
+        --status /var/run/openvpn.status 5 --writepid /var/run/openvpn.pid \
+        --dev tun0
