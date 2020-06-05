15d8dbe9d473553f6246cba6a7f79b5252e6fe4f
Alexander Ursu <alexander.ursu@gmail.com>
2020-06-05 14:59:11 +0200

    Update vpnonly-eno1.sh

--- vpnonly-eno1.sh
+++ vpnonly-eno1.sh
@@ -2,6 +2,18 @@
 IFACE=eno1
 DNS=192.168.178.1
 LOCALNET=192.168.178.0/24
+# nl2-ovpn.pointtoserver.com.       100 IN	A	188.72.98.4
+# nl2-ovpn.pointtoserver.com.       100 IN	A	172.94.19.4
+# nl2-ovpn.pointtoserver.com.       100 IN	A	178.170.137.4
+# bg2-ovpn.pointtoserver.com.       81  IN	A	37.120.152.52
+# usfl2-ovpn.pointtoserver.com.     74  IN	A	37.230.169.4
+# usfl2-ovpn.pointtoserver.com.     74  IN	A	172.94.108.4
+# tr2-ovpn.pointtoserver.com.       75  IN	A	172.94.49.4
+# ae2-ovpn.pointtoserver.com.       69  IN	A	104.37.6.4
+# de2-ovpn-tcp.pointtoserver.com.   100 IN  A   172.111.203.4
+# de2-ovpn-tcp.pointtoserver.com.   100 IN  A   46.243.238.4
+# de2-ovpn-tcp.pointtoserver.com.   100 IN  A   172.94.11.4
+VPNGWS=(188.72.98.4 172.94.19.4 178.170.137.4 37.120.152.52 37.230.169.4 172.94.108.4 172.94.49.4 104.37.6.4 172.111.203.4 46.243.238.4 172.94.11.4)
 
 iptables -F
 iptables -X
@@ -21,27 +33,11 @@ ip6tables -P FORWARD DROP
 sysctl net.ipv6.conf.default.disable_ipv6=1
 sysctl net.ipv6.conf.all.disable_ipv6=1
 
-# nl2-ovpn-tcp.pointtoserver.com.	120 IN	CNAME	nl2-ovpn.pointtoserver.com.
-# nl2-ovpn.pointtoserver.com. 100	IN	A	188.72.98.4
-# nl2-ovpn.pointtoserver.com. 100	IN	A	172.94.19.4
-# nl2-ovpn.pointtoserver.com. 100	IN	A	178.170.137.4
-# bg2-ovpn.pointtoserver.com. 81	IN	A	37.120.152.52
-# usfl2-ovpn.pointtoserver.com. 74 IN	A	37.230.169.4
-# usfl2-ovpn.pointtoserver.com. 74 IN	A	172.94.108.4
-# tr2-ovpn.pointtoserver.com. 75	IN	A	172.94.49.4
-# ae2-ovpn.pointtoserver.com. 69	IN	A	104.37.6.4
-# de2-ovpn-tcp.pointtoserver.com. 100 IN  A       172.111.203.4
-# de2-ovpn-tcp.pointtoserver.com. 100 IN  A       46.243.238.4
-# de2-ovpn-tcp.pointtoserver.com. 100 IN  A       172.94.11.4
-for p in 188.72.98.4 172.94.19.4 178.170.137.4 37.120.152.52 37.230.169.4 172.94.108.4 172.94.49.4 104.37.6.4 172.111.203.4 46.243.238.4 172.94.11.4; do 
+# VPN gateways IPs
+for p in ${VPNGWS[@]}; do 
     iptables -A INPUT -s $p/32 -m state --state ESTABLISHED -j ACCEPT
     iptables -A OUTPUT -d $p/32 -p tcp -m tcp --dport 80 -j ACCEPT
 done
-
-#for n in 172.94.19.128/26; do
-#    iptables -A INPUT -s $n -m state --state ESTABLISHED -j ACCEPT
-#    iptables -A OUTPUT -d $n -p tcp -m tcp --dport 80 -j ACCEPT
-#done
 
 # allow loopback traffic
 iptables -A INPUT -i lo -j ACCEPT
